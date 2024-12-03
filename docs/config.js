ff38108313678cbcd1be79348f8c51345d534dc9
Your Name <your-email@example.com>
2024-12-03 13:19:52 +0000

    Switch to HTTP for development and simplify nginx configuration

--- docs/config.js
+++ docs/config.js
@@ -1,2 +1,4 @@
-const API_BASE_URL = 'https://152.93.129.206/api';
+const API_BASE_URL = window.location.hostname === 'localhost' 
+    ? 'http://localhost:5000/api'
+    : 'http://152.93.129.206/api';
 const QR_API_BASE_URL = API_BASE_URL;
